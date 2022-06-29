5f2b8a171f329299e853bd8eb5946e62a9a60467
Ruslan Baratov <x@ruslo.dev>
2022-06-29 21:36:12 +0800

    Get the laser info

--- app/src/main/java/com/olyseus/i_seed/control/MainActivity.java
+++ app/src/main/java/com/olyseus/i_seed/control/MainActivity.java
@@ -11,11 +11,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -28,21 +30,42 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+import dji.common.camera.LaserError;
+import dji.common.camera.LaserMeasureInformation;
+import dji.common.error.DJIError;
+import dji.common.error.DJISDKError;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.base.BaseComponent;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.camera.Lens;
+import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKInitEvent;
+import dji.sdk.sdkmanager.DJISDKManager;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "MainActivity";
     private GoogleMap gMap;
     private ScheduledExecutorService executorService;
     private Handler handler;
+    private boolean sdkRegistrationStarted = false;
+    private static boolean useBridge = false;
+    private Aircraft aircraft = null;
 
     enum State {
         NO_PERMISSIONS,
         MAP_NOT_READY,
+        NOT_REGISTERED,
+        REGISTRATION_FAILED,
         NO_PRODUCT,
+        LASER_OFF,
+        WAIT_LASER,
+        LASER_ERROR,
         ONLINE
     }
 
     private AtomicReference<State> state = new AtomicReference<State>(State.NO_PERMISSIONS);
-    private AtomicReference<Double> laserDistance = new AtomicReference<Double>(0.0);
+    private AtomicReference<Float> laserDistance = new AtomicReference<Float>(0.0F);
 
     private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
             Manifest.permission.BLUETOOTH,
@@ -89,22 +112,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         executorService.scheduleAtFixedRate(pollRunnable, 0, 1, TimeUnit.SECONDS);
     }
 
+    private void updateState(State newState) {
+        state.set(newState);
+        handler.sendEmptyMessage(0);
+    }
+
     private void pollJob() {
         if (!missingPermission.isEmpty()) {
-            state.set(State.NO_PERMISSIONS);
-            handler.sendEmptyMessage(0);
+            updateState(State.NO_PERMISSIONS);
             return;
         }
 
         if (gMap == null) {
-            state.set(State.MAP_NOT_READY);
-            handler.sendEmptyMessage(0);
+            updateState(State.MAP_NOT_READY);
             return;
         }
 
-        // FIXME
-        state.set(State.ONLINE);
-        handler.sendEmptyMessage(0);
+        if (!sdkRegistrationStarted) {
+            sdkRegistrationStarted = true;
+            updateState(State.NOT_REGISTERED);
+            startSDKRegistration();
+            return;
+        }
+
+        if (state.get() == State.LASER_OFF) {
+            enableLaser();
+            return;
+        }
     }
 
     private void updateUIState() {
@@ -116,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         Button laserStatus = findViewById(R.id.laserStatus);
         if (state.get() == State.ONLINE) {
-            laserStatus.setText("Laser: " + laserDistance.get());
+            laserStatus.setText("Laser: " + String.format("%.1f", laserDistance.get()));
         }
         else {
             laserStatus.setText("Laser: N/A");
@@ -127,8 +161,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         switch (state.get()) {
             case NO_PERMISSIONS:
             case MAP_NOT_READY:
+            case NOT_REGISTERED:
+            case REGISTRATION_FAILED:
             case NO_PRODUCT:
                 return android.R.color.holo_red_light;
+            case LASER_OFF:
+            case WAIT_LASER:
+            case LASER_ERROR:
+                return android.R.color.holo_orange_light;
             case ONLINE:
                 return android.R.color.holo_green_dark;
             default:
@@ -143,8 +183,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 return "No permissions";
             case MAP_NOT_READY:
                 return "Map not ready";
+            case NOT_REGISTERED:
+                return "Not registered";
+            case REGISTRATION_FAILED:
+                return "Registration failed";
             case NO_PRODUCT:
                 return "No product";
+            case LASER_OFF:
+                return "Laser is off";
+            case WAIT_LASER:
+                return "Waiting laser";
+            case LASER_ERROR:
+                return "Laser error";
             case ONLINE:
                 return "Online";
             default:
@@ -198,6 +248,121 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // If there is enough permission, we will start the registration
         if (!missingPermission.isEmpty()) {
             showToast("Permissions are missing");
+        }
+    }
+
+    private void startSDKRegistration() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                DJISDKManager.getInstance().registerApp(MainActivity.this.getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
+                    @Override
+                    public void onRegister(DJIError djiError) {
+                        if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
+                            if (useBridge) {
+                                DJISDKManager.getInstance().enableBridgeModeWithBridgeAppIP("192.168.0.227");
+                            }
+                            DJISDKManager.getInstance().startConnectionToProduct();
+                            updateState(State.NO_PRODUCT);
+                            return;
+                        }
+
+                        // Check bundle id and network connection
+                        Log.e(TAG, "Registration failed: " + djiError.getDescription());
+                        sdkRegistrationStarted = false;
+                        updateState(State.REGISTRATION_FAILED);
+                    }
+
+                    @Override
+                    public void onProductDisconnect() {
+                        Log.e(TAG, "Product disconnect");
+                        aircraft = null;
+                        updateState(State.NO_PRODUCT);
+                    }
+
+                    @Override
+                    public void onProductConnect(BaseProduct baseProduct) {
+                        assert (baseProduct instanceof Aircraft);
+                        aircraft = (Aircraft) baseProduct;
+                        assert (aircraft != null);
+                        updateState(State.LASER_OFF);
+                    }
+
+                    @Override
+                    public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {}
+
+                    @Override
+                    public void onProductChanged(BaseProduct baseProduct) {
+                        assert(false);
+                    }
+
+                    @Override
+                    public void onDatabaseDownloadProgress(long l, long l1) {}
+
+                    @Override
+                    public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
+                                                  BaseComponent newComponent) {}
+                });
+            }
+        });
+    }
+
+    private void enableLaser() {
+        List<Camera> cameras = aircraft.getCameras();
+        if (cameras.isEmpty()) {
+            return;
+        }
+        for (Camera camera : cameras) {
+            Log.i(TAG, "Camera: " + camera.getDisplayName());
+            camera.setLaserEnabled(true, null);
+            camera.getLaserEnabled(new CommonCallbacks.CompletionCallbackWith<Boolean>() {
+                @Override
+                public void onSuccess(Boolean aBoolean) {
+                    assert(aBoolean == true);
+                    Log.i(TAG, "Laser is ON");
+                    updateState(State.WAIT_LASER);
+                    setupMeasurementCallback(camera);
+                }
+
+                @Override
+                public void onFailure(DJIError djiError) {}
+            });
+        }
+    }
+
+    private void setupMeasurementCallback(Camera camera) {
+        for (Lens lens : camera.getLenses()) {
+            lens.setLaserMeasureInformationCallback(new LaserMeasureInformation.Callback() {
+                @Override
+                public void onUpdate(LaserMeasureInformation laserMeasureInformation) {
+                    float targetDistance = laserMeasureInformation.getTargetDistance();
+                    LaserError laserError = laserMeasureInformation.getLaserError();
+                    if (laserError == LaserError.TOO_FAR) {
+                        Log.e(TAG, "Laser is too far");
+                        updateState(State.LASER_ERROR);
+                        return;
+                    }
+                    if (laserError == LaserError.TOO_CLOSE) {
+                        Log.e(TAG, "Laser is too close");
+                        updateState(State.LASER_ERROR);
+                        return;
+                    }
+                    if (laserError == LaserError.NO_SIGNAL) {
+                        Log.e(TAG, "Laser no signal");
+                        updateState(State.LASER_ERROR);
+                        return;
+                    }
+                    if (laserError == LaserError.UNKNOWN) {
+                        Log.e(TAG, "Laser state is unknown");
+                        updateState(State.LASER_ERROR);
+                        return;
+                    }
+                    assert(laserError == LaserError.NORMAL);
+                    updateState(State.ONLINE);
+                    laserDistance.set(targetDistance);
+                    Log.i(TAG, "Laser info: " + laserMeasureInformation);
+                }
+            });
         }
     }
 
